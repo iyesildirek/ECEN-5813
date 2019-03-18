1e033d8fdc9fbe2de3d773fceb99036b660c398e
iyesildirek <26639277+iyesildirek@users.noreply.github.com>
2019-03-18 18:08:39 -0500

    Finished Implementation

--- Circular Buffer/ring.c
+++ Circular Buffer/ring.c
@@ -15,13 +15,18 @@
 *****************************************************************************/
 
 /**
-* @file ring_test.c
+* @file ring.c
 * @brief This source file contains a c program to implement a circular ring buffer.
 *
 * @author Ismail Yesildirek
 * @date March 18 2019
 * @version 1.0
 *
+*/
+
+/*
+* Reference "Implementing Circular Buffer in C" for implementation strategy. 
+* https://embedjournal.com/implementing-circular-buffer-embedded-c/
 */
 
 typedef struct
@@ -60,10 +65,10 @@ int main(void)
 		(buffer_struct->count != NULL) || (buffer_struct->Ini != NULL) || (buffer_struct->Length != NULL) || \
 		(buffer_struct->Outi != NULL))
 	{
-		while (queueFull == FALSE)
+		printf("This program creates a circular buffer of size %d\n", bufferSize);
+		while (1)
 		{
-			printf("This program creates a circular buffer of size %d\n", bufferSize);
-			printf("Please enter 0 to insert, 1 to remove or 2 to check the number of entries in the circular buffer\n");
+			printf("Please enter 0 to insert, 1 to remove, 2 to check the number of entries in the circular buffer or 3 to exit\n");
 			scanf(" %d", &userInput);
 			if (userInput == 0)
 			{
@@ -75,12 +80,17 @@ int main(void)
 			}
 			else if (userInput == 1)
 			{
-				rmStatus = rm(buffer_struct, buffer_struct->Buffer);
+				rmStatus = rm(buffer_struct, &(buffer_struct->Buffer));
 			}
 			else if (userInput == 2)
 			{
 				entryStatus = entries(buffer_struct);
-				printf("The number of entries are %d", entryStatus);
+				printf("The number of entries are %d\n", entryStatus);
+			}
+			else if (userInput == 3)
+			{
+				printf("Good Bye!\n");
+				return 0;
 			}
 			else
 			{
@@ -115,21 +125,32 @@ int insert(ring_t *ring, char data)
 	if ((ring != NULL) || (ring->Buffer != NULL) || (ring->circularQueue != NULL) || \
 		(ring->count != NULL) || (ring->Ini != NULL) || (ring->Length != NULL) || \
 		(ring->Outi != NULL))
-	{	
-		if ((ring->Ini - ring->Outi) < ring->Length)
-		{
-			ring->circularQueue[ring->Ini] = data; /*Add data to queue position Ini*/
-			ring->count++;	/*number of insert()*/
-			ring->Ini++; /*increment queue index*/
-			ring->Buffer = &(ring->circularQueue[ring->Outi]);
-			/* Buffer ptr is now the same as the last term of the queue*/
-		}
-		if (ring->Outi != ring->Ini)
-		{
+	{
+		/*Next head index*/
+		int nextIndex = ring->Ini + 1;
 
+		/*Check if buffer is full*/
+		if (nextIndex == ring->Outi)
+		{
+		printf("Circular Buffer is Full. Remove an item with the rm()\n");
+		return -1;
 		}
+
+		/*Check if head index needs to circle back in buffer*/
+		else if (nextIndex >= ring->Length)
+		{
+			nextIndex = 0;
+		}
+
+		else
+		{
+			//do nothing
+		}
+		ring->circularQueue[ring->Ini] = data; /*Add data to queue position Ini*/
+		ring->count++;	/*number of insert()*/
+		ring->Ini++; /*increment queue index*/
 		return 0;
-	}
+}
 	else
 	{
 		printf("NULL pointer provided\n");
@@ -143,7 +164,31 @@ int rm(ring_t *ring, char *data)
 		(ring->count != NULL) || (ring->Ini != NULL) || (ring->Length != NULL) || \
 		(ring->Outi != NULL))
 	{
-		printf("In RM function() \n");
+		/*Next tail index*/
+		int nextIndex = ring->Outi + 1;
+		
+		/*Check if buffer is empty*/
+		if (ring->Ini == ring->Outi)
+		{
+			printf("Circular Buffer is Empty. Add an item with the insert()\n");
+			return -1;
+		}
+
+		/*Check if tail index needs to circle back in buffer*/
+		else if (nextIndex >= ring->Length)
+		{
+			nextIndex = 0;
+		}
+		else
+		{
+			//do nothing
+		}
+
+		/*Read char and store in *Buffer prior to clearing buffer[Ouit]*/
+		*data = ring->circularQueue[ring->Outi];
+		ring->circularQueue[ring->Outi] = '0'; /*clear buffer*/
+		ring->Outi++;
+		ring->count--;	/*Reduce number of insert()*/
 		return 0;
 	}
 	else
@@ -159,7 +204,6 @@ int entries(ring_t *ring)
 		(ring->count != NULL) || (ring->Ini != NULL) || (ring->Length != NULL) || \
 		(ring->Outi != NULL))
 	{
-		printf("In entries function() \n");
 		int temp;
 		temp = ring->count;
 		return temp;
--- Circular Buffer/ring.h
+++ Circular Buffer/ring.h
@@ -3,6 +3,28 @@
 ** Circular buffer
 ** ECEN5813
 **========================================================================*/
+
+/*****************************************************************************
+* Copyright (C) 2019 by Ismail Yesildirek
+*
+* Redistribution, modification or use of this software in source or binary
+* forms is permitted as long as the files maintain this copyright. Users are
+* permitted to modify this and use it to learn about the field of embedded
+* software. Ismail Yesildirek and the University of Colorado are not
+* liable for any misuse of this material.
+*
+*****************************************************************************/
+
+/**
+* @file ring.h
+* @brief This header file provides the ring buffer prototypes.
+*
+* @author Ismail Yesildirek
+* @date March 18 2019
+* @version 1.0
+*
+*/
+
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
